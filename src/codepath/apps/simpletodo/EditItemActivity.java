a1db02e57f73cedbcbe0e2ea6a2d5d160889ac60
sandramngo <sandramngo@gmail.com>
2014-05-04 17:16:10 -0700

    remove unneeded methods

--- src/codepath/apps/simpletodo/EditItemActivity.java
+++ src/codepath/apps/simpletodo/EditItemActivity.java
@@ -3,8 +3,6 @@ package codepath.apps.simpletodo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -25,26 +23,6 @@ public class EditItemActivity extends Activity {
 		etEditItem = (EditText) findViewById(R.id.etEditItem);
 		etEditItem.setText(todoItem);
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.edit_item, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 	
 	public void saveTodoItem(View v) {
